c8b876a489b1cc3e8c5d2a3ae16f277a1d1da654
Emil Kabanov <kabemil@gmail.com>
2020-10-12 09:16:33 +0300

    добавил все файлы

---
+++ Lesson-2/tmp.py
@@ -0,0 +1,13 @@
+my_analys = {'название': ['комп', 'скан'], 'цена': ['2', '3'], 'количество': ['3', '4'], 'eд': ['шт', 'шт']}
+print("my_analys =", my_analys)
+
+for keys in my_analys.keys():
+    new_analys = dict({keys:''})
+    print(new_analys)
+for keys in my_analys.values():
+    new_analys[0]
+
+#print(my_analys.values())
+
+#for el in my_analys.values():
+#    print(el)
